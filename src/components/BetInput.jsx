0a963669d3870677c2e64cbf63f0e500a033d929
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-05-19 20:19:06 +0200

    update

---
+++ src/components/BetInput.jsx
@@ -0,0 +1,32 @@
+import { Input } from "@heroui/input";
+import { useState } from "react";
+import BlackjackButton from './BlackjackButton';
+
+export default function BetInput({ placeBet, playerBalance }) {
+    const [amount, setAmount] = useState("");
+
+    const handleClick = () => {
+        const bet = parseInt(amount);
+        if (!isNaN(bet)) {
+            placeBet(bet);
+            setAmount("");
+        }
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center gap-2 w-full">
+            <Input
+                label="Enter Bet"
+                type="number"
+                min={50}
+                max={playerBalance}
+                variant="underlined"
+                value={amount}
+                step={50}
+                onChange={(e) => setAmount(e.target.value)}
+                className="w-[150px] mb-2"
+            />
+            <BlackjackButton styling="default" text="Place Bet" onClick={handleClick} />
+        </div>
+    );
+}
